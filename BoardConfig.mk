177474ccb53b18a9ca04e0b8d4ae7c5697f37ed7
Zhao Wei Liew <zhaoweiliew@gmail.com>
2016-04-05 19:33:36 +0800

    sumire: Append DTB to kernel
    This is how our stock kernel currently does it.
    
    Change-Id: I814606e2485ef29cebe778fe116f49a466f10ea8

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -47,9 +47,10 @@ TARGET_NO_BOOTLOADER := true
 BOARD_DTBTOOL_ARGS := -3
 BOARD_KERNEL_BASE := 0x00000000
 BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x237 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 boot_cpus=0-5 dwc3_msm.hvdcp_max_current=1500 dwc3_msm.prop_chg_detect=Y coherent_pool=2M androidboot.selinux=permissive
+BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
 BOARD_KERNEL_PAGESIZE := 4096
-BOARD_KERNEL_SEPARATED_DT := true
 BOARD_MKBOOTIMG_ARGS := --kernel_offset 0x00008000 --ramdisk_offset 0x02000000 --tags_offset 0x00000100
+TARGET_KERNEL_APPEND_DTB := true
 TARGET_KERNEL_ARCH := arm64
 TARGET_KERNEL_CONFIG := cyanogenmod_sumire_defconfig
 TARGET_KERNEL_CROSS_COMPILE_PREFIX := aarch64-linux-android-
