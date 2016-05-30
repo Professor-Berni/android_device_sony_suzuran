7278eb530a4f0d4a3c4401d43f75b0776d202cce
Olivier Karasangabo <olivier@cyanogenmod.org>
2016-05-30 21:01:33 +0200

    ivy: Add missing bluetooth config

--- device.mk
+++ device.mk
@@ -96,6 +96,10 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/audio_policy.conf:system/etc/audio_policy.conf \
     $(LOCAL_PATH)/audio/mixer_paths.xml:system/etc/mixer_paths.xml
 
+# Bluetooth
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/bluetooth/bt_vendor.conf:system/etc/bluetooth/bt_vendor.conf
+
 # Charger
 PRODUCT_PACKAGES += charger_res_images
 
