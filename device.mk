a9c5c364028b1c8f4162974dcd31eb922f587446
Nickolay Semendyaev <agent00791@gmail.com>
2016-12-28 04:29:47 +0600

    Update ramdisk, remove sensors script

--- device.mk
+++ device.mk
@@ -87,7 +87,6 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/rootdir/init.qcom.power.rc:root/init.qcom.power.rc \
     $(LOCAL_PATH)/rootdir/init.qcom.usb.rc:root/init.qcom.usb.rc \
     $(LOCAL_PATH)/rootdir/init.qcom.rc:root/init.qcom.rc \
-    $(LOCAL_PATH)/rootdir/init.qcom-sensor.sh:root/init.qcom-sensor.sh \
     $(LOCAL_PATH)/rootdir/ueventd.qcom.rc:root/ueventd.qcom.rc
 
 # Audio configuration
