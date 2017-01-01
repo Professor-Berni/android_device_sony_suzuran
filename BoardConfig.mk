9075a6a9e74d895c7d355c897eb647f1f98f222d
Nickolay Semendyaev <agent00791@gmail.com>
2017-01-02 03:34:11 +0600

    Initial selinux rules. Because 7.1.1 sometimes block even in permission =\

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -147,8 +147,8 @@ TARGET_RIL_VARIANT := caf
 # SELinux
 include device/qcom/sepolicy/sepolicy.mk
 
-#BOARD_SEPOLICY_DIRS += \
-#    $(DEVICE_PATH)/sepolicy
+BOARD_SEPOLICY_DIRS += \
+    $(DEVICE_PATH)/sepolicy
 
 # Wifi
 BOARD_WLAN_DEVICE           := bcmdhd
