fc1890d2a36116610d79eefb503d3a0ef99a0934
Giulio Cervera <giulio.cervera@cyanogenmod.org>
2017-01-06 11:41:51 +0100

    thermal-engine: add missing symbol

---
+++ libshim/libgui_shim.cpp
@@ -0,0 +1,9 @@
+#include <sys/types.h>
+
+extern "C" {
+  ssize_t _ZN7android13SensorManager13getSensorListEPPKPKNS_6SensorE(void* thiz, void* list);
+
+  ssize_t _ZNK7android13SensorManager13getSensorListEPPKPKNS_6SensorE(void* thiz, void* list) {
+    return _ZN7android13SensorManager13getSensorListEPPKPKNS_6SensorE(thiz, list);
+  }
+}
