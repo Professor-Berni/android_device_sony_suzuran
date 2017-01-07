b5a7196a4cb4a1055a4dc3a8fe272989a4338f0e
Giulio Cervera <giulio.cervera@cyanogenmod.org>
2017-01-07 23:53:28 +0600

    refactor power script
    * remove power script init (.rc) and use a smarter .sh
      for now is plain copy from angler, our tuning will be restored
      in future patch
    * move missing tuning part from power rc (swap,ksm,ecc...) to main
      rc

--- device.mk
+++ device.mk
@@ -86,10 +86,10 @@ PRODUCT_PACKAGES += \
 # init
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/rootdir/fstab.qcom:root/fstab.qcom \
-    $(LOCAL_PATH)/rootdir/init.qcom.power.rc:root/init.qcom.power.rc \
     $(LOCAL_PATH)/rootdir/init.qcom.usb.rc:root/init.qcom.usb.rc \
     $(LOCAL_PATH)/rootdir/init.qcom.rc:root/init.qcom.rc \
-    $(LOCAL_PATH)/rootdir/ueventd.qcom.rc:root/ueventd.qcom.rc
+    $(LOCAL_PATH)/rootdir/ueventd.qcom.rc:root/ueventd.qcom.rc \
+    $(LOCAL_PATH)/rootdir/bin/init.qcom.power.sh:system/bin/init.qcom.power.sh
 
 # Audio configuration
 PRODUCT_COPY_FILES += \
