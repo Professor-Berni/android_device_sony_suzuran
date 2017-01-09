9c6fc3313d50132fe404b6ee6b17137be61b8887
Christian Oder <myself5@carbonrom.org>
2017-01-09 20:28:02 +0100

    Revert "Improve setup-makefiles script"
    This reverts commit 70598c11618d5d7777c2293c460cce2b3fa26ee5.

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -1,43 +1,11 @@
 #!/bin/bash
 
-VENDOR=sony
-DEVICE=ivy
 OUTDIR=vendor/$VENDOR/$DEVICE
 MAKEFILE=../../../$OUTDIR/$DEVICE-vendor-blobs.mk
 
-(cat << EOF) > $MAKEFILE
-# Copyright (C) 2016 The CyanogenMod Project
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
-PRODUCT_COPY_FILES += \\
-EOF
-
-LINEEND=" \\"
-COUNT=`cat proprietary-files.txt | grep -v ^# | grep -v ^$ | wc -l | awk {'print $1'}`
-for FILE in `cat proprietary-files.txt | grep -v ^# | grep -v ^$ | sed -e 's#^/system/##g'`; do
-    COUNT=`expr $COUNT - 1`
-    if [ $COUNT = "0" ]; then
-        LINEEND=""
-    fi
-    if [[ ! "$FILE" =~ ^-.* ]]; then
-        FILE=`echo $FILE | sed -e "s/^-//g"`
-        echo "    $OUTDIR/proprietary/$FILE:system/$FILE$LINEEND" >> $MAKEFILE
-    fi
-done
+if [ $BOARDCONFIGVENDOR != "true" ]; then
 
 (cat << EOF) > ../../../$OUTDIR/$DEVICE-vendor.mk
-
 # Copyright (C) 2016 The CyanogenMod Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -118,3 +86,5 @@ include \$(BUILD_PREBUILT)
 endif
 
 EOF
+
+fi
