e2e7cfea71235826205364dd0215c1ae889480c1
Olivier Karasangabo <olivier@lineageos.org>
2017-11-02 22:17:58 +0100

    suzuran: Use Lineage vendor
    Change-Id: I9dec5304fc62785a40233998bc0906c176d6465e

--- lineage.mk
+++ lineage.mk
@@ -17,7 +17,7 @@
 $(call inherit-product, device/sony/suzuran/full_suzuran.mk)
 
 # Inherit some common CM stuff
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 PRODUCT_NAME := lineage_suzuran
 
