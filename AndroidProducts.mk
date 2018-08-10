cf33cc6cbaa2d250d42b93add104aff1b76f2152
Bruno Martins <bgcngm@gmail.com>
2018-08-10 14:26:16 +0100

    suzuran: Consider lineage_suzuran product via the AOSP way
    Change-Id: Iabdaacb8f29b2e268b2d076077e4c042a6198174

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2015 The CyanogenMod Project
+# Copyright (C) 2018 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -12,18 +12,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-#
 
-$(call inherit-product, device/sony/suzuran/full_suzuran.mk)
-
-# Inherit some common CM stuff
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
-
-PRODUCT_NAME := lineage_suzuran
-
-# Use the latest approved GMS identifiers
-
-PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRODUCT_NAME=suzuran \
-    BUILD_FINGERPRINT=Sony/suzuran/suzuran:7.1.1/N-MR1-KITAKAMI-170920-0229/1:user/dev-keys \
-    PRIVATE_BUILD_DESC="suzuran-user 7.1.1 N-MR1-KITAKAMI-170920-0229 1 dev-keys"
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/lineage_suzuran.mk
---
+++ lineage_suzuran.mk
@@ -0,0 +1,29 @@
+#
+# Copyright (C) 2020 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+$(call inherit-product, device/sony/suzuran/full_suzuran.mk)
+
+# Inherit some common LineageOS stuff
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
+PRODUCT_NAME := lineage_suzuran
+
+# Use the latest approved GMS identifiers
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME=suzuran \
+    BUILD_FINGERPRINT=Sony/suzuran/suzuran:7.1.1/N-MR1-KITAKAMI-170920-0229/1:user/dev-keys \
+    PRIVATE_BUILD_DESC="suzuran-user 7.1.1 N-MR1-KITAKAMI-170920-0229 1 dev-keys"
