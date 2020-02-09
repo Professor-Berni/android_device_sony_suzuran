64c345033aece71a8bb0a3f5f29f7a7bbec9854f
TARKZiM <tom8476oo@gmail.com>
2020-02-09 23:34:38 +0800

    suzuran: Build Biometrics Fingerprint 2.1 HAL

--- device.mk
+++ device.mk
@@ -72,4 +72,4 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.fingerprint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.fingerprint.xml
 
 PRODUCT_PACKAGES += \
-    fingerprintd
+    android.hardware.biometrics.fingerprint@2.1-service
