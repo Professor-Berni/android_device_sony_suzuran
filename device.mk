b5a6031074cb3a93a362e6b9a35ae321a907dad3
TARKZiM <tom8476oo@gmail.com>
2020-11-04 13:44:53 +0800

    suzuran: Fingerprint HAL 2.0 -> 2.1

--- device.mk
+++ device.mk
@@ -75,4 +75,4 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.fingerprint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.fingerprint.xml
 
 PRODUCT_PACKAGES += \
-    android.hardware.biometrics.fingerprint@2.0-service.kitakami
+    android.hardware.biometrics.fingerprint@2.1-service.kitakami
