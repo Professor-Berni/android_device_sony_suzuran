212cf4da40c0e3477ed71ac6d004727e6ba38d3a
Bernhard Thoben <berni-the-bear@web.de>
2021-10-02 16:24:18 +0200

    suzuran: Adapted commit d79435d to LineageOS 15.1.
    Change-Id: I22b756b6124b7a5e95697c8d95de49c9f2337376

--- init_suzuran/init_suzuran.cpp
+++ init_suzuran/init_suzuran.cpp
@@ -35,7 +35,7 @@
 #include "property_service.h"
 #include "util.h"
 
-using android::init::ImportKernelCmdline;
+using android::init::import_kernel_cmdline;
 
 constexpr auto LTALABEL_PATH = "/lta-label/pages/menu.htm";
 
@@ -94,7 +94,5 @@ static void import_kernel_nv(const std::string& key,
 
 void vendor_load_properties()
 {
-    ImportKernelCmdline([&](const std::string& key, const std::string& value) {
-        import_kernel_nv(key, value, false);
-    });
+    import_kernel_cmdline(0, import_kernel_nv);
 }
